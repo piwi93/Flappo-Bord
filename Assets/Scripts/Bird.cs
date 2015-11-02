879133470c82020a1dbdf7db334e01a2ca20a455
Emanuel Laporta <emanuel.laporta@outlook.com>
2015-11-02 18:53:48 -0300

    Tiny PowerUp

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Bird : MonoBehaviour {
@@ -16,7 +16,7 @@ public class Bird : MonoBehaviour {
 	bool hasPlayed = false;
 
 	string[] states = {"normal", "unstopable", "tiny", "killer"};
-	Vector3 powerupPosition = Vector3.zero;
+	Vector3 xPosition = Vector3.zero;
 
 	AudioSource bloob;
 	AudioSource peow;
@@ -24,14 +24,35 @@ public class Bird : MonoBehaviour {
 	AudioSource boup;
 
 
+
+	//Set Bird to Normal
+	void setBirdToNormal(){
+		
+		//Set normal state
+		birdState = states[0];
+		
+		//Set scale
+		transform.localScale = new Vector3(6.064453F, 7.314585F, 1F);
+		
+		//Set mass values
+		vForce.y = -18;
+		vJump.y = 7;
+		maxSpeed = 7;
+		
+		//Set position
+		xPosition = this.gameObject.transform.position;
+		xPosition.x = -4.73F;
+		transform.position = xPosition;
+	}
+
+
+
 	//Use this for initialization
 	void Start(){
 
+		//Set thhe public vars
     	vForce.y = force;
     	vJump.y = jump;
-
-    	//Set normal state to bird
-    	birdState = states[0];
 
     	//Set all audio sources
     	AudioSource[] allMyAudioSources = GetComponents<AudioSource>();
@@ -40,46 +61,16 @@ public class Bird : MonoBehaviour {
 		pipe = allMyAudioSources[2];
 		boup = allMyAudioSources[3];
 
+		//Set bird
+		setBirdToNormal();
 	}
 
-	//PowerUp timer to get normal
-	IEnumerator powerUpTimer() {
 
-		//Restore Values after 10'
-		yield return new WaitForSeconds(9F);
-
-		//Loosing powerup sound
-		if(hasPlayed == false){
-			pipe.Play();
-			hasPlayed = true;
-			yield return new WaitForSeconds(0.5F);
-		}
-
-		//For Unstopable
-		if(birdState == states[1]){
-			transform.localScale = new Vector3(6.064453F, 7.314585F, 1F);
-			powerupPosition.x = 1.5F;
-			transform.position -= powerupPosition;
-
-			//Restore mass values
-			vForce.y = -18;
-			vJump.y = 7;
-			maxSpeed = 7;
-		}
-
-		//Normal again
-		birdState = states[0];
-	}
 
 	//Update is called once per frame
 	void Update(){
 		
-		//Check if I got any power up
-		if(birdState != states[0]){
-			StartCoroutine(powerUpTimer());
-		}
-
-        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+		if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
             flap = true;
 
             //bloob sound
@@ -92,28 +83,28 @@ public class Bird : MonoBehaviour {
 	//Update more slow than normal
 	void FixedUpdate(){
 
+		//Bird movement
 		speed += vForce * Time.deltaTime;
-
 		if(flap == true){
 			flap = false;
 			speed.y = vJump.y;
 		}
-
 		transform.position += speed * Time.deltaTime;
 
 		//Transform angle when bird is falling down & is flappying
 		float rotation = 0;
-
 		if(speed.y >= 0){
 			rotation = Mathf.Lerp (0, 40, speed.y / maxSpeed);
 		}
 		else{
 			rotation = Mathf.Lerp (0, -70, -speed.y / maxSpeed);
 		}
-
 		transform.rotation = Quaternion.Euler(0, 0, rotation);
 	}
 
+
+
+/****** COLLISIONS ******/
 
 	//When Collide
 	void OnCollisionEnter2D(Collision2D Collission){
@@ -130,19 +121,19 @@ public class Bird : MonoBehaviour {
 
 		//Or Im not unstopable and I collide with the rest
 		else if( birdState != states[1] ){
-			//bloob sound
+			//peow sound
 			peow.Play();
 
 			//Restart Current Scene (Pause?)
 			Application.LoadLevel(Application.loadedLevel);
 		}
 
-		//Else Im unstopable bitch, but better if I dont collide with the ground 
+		//Else: Im unstopable bitch, but better if I dont collide with the ground 
 		else{
-			if(Collission.gameObject.name == "Ground"){
+			//peow sound
+			peow.Play();
 
-				//bloob sound
-				peow.Play();
+			if(Collission.gameObject.name == "Ground"){
 
 				//Restart Current Scene (Pause?)
 				Application.LoadLevel(Application.loadedLevel);
@@ -152,24 +143,45 @@ public class Bird : MonoBehaviour {
 	}
 
 
+
+/****** POWER UPS ******/
+
+	//PowerUp timer
+	IEnumerator powerUpTimer(){
+		
+		//Restore Values after 7'
+		yield return new WaitForSeconds(7F);
+		setBirdToNormal();
+		
+		//Loosing powerup sound
+		if(hasPlayed == false){
+			pipe.Play();
+			hasPlayed = true;
+			yield return new WaitForSeconds(0.5F);
+		}
+	}
+
+
 	//Used in Unstopable Power Up
 	IEnumerator waitGrow(){
 		
-        yield return new WaitForSeconds(0.200F);
-
+		yield return new WaitForSeconds(0.200F);
+		
 		//Change position & scale
-		powerupPosition.x = 1.5F;
-		transform.position += powerupPosition;
-        transform.localScale = new Vector3(32, 32, 1);
-
+		xPosition = this.gameObject.transform.position;
+		xPosition.x = -3.2F;
+		transform.position = xPosition;
+		transform.localScale = new Vector3(32, 32, 1);
+		
 		//Changes mass value
 		vForce.y = -20;
 		vJump.y = 6;
 		maxSpeed = 20;
-    }
+	}
 
-	//When Trigger
-	void OnTriggerEnter2D(Collider2D Collider){
+
+	//When Trigger with a powerup
+	void OnTriggerEnter2D(Collider2D Collider){;
 		
 		//Unstopable Power Up
 		if(Collider.gameObject.name == "UnstopablePowerUp"){
@@ -183,8 +195,27 @@ public class Bird : MonoBehaviour {
 			
 			//Delay of 0.3ms and then get 2nd size
 			StartCoroutine(waitGrow());
+
+			StartCoroutine(powerUpTimer());
 		}
 
+		//Tiny Power Up
+		else if(Collider.gameObject.name == "TinyPowerUp"){
+
+			//Set tiny state
+			birdState = states[2];
+			hasPlayed= false;
+
+			//Size
+			transform.localScale = new Vector3(4, 4, 1);
+
+			//Mass value
+			vForce.y = -14;
+			vJump.y = 5;
+			maxSpeed = 5;
+
+			StartCoroutine(powerUpTimer());
+		}
 	}
 
-}
+} //End
